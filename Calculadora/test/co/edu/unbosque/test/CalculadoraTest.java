acaee52541ffe8295c59ea80f58ee2765b78ce9c
dromeromo <dromeromo@unbosque.edu.co>
2015-01-30 13:02:53 -0500

    se creo la clase claculadora y el metodo suma

--- Calculadora/test/co/edu/unbosque/test/CalculadoraTest.java
+++ Calculadora/test/co/edu/unbosque/test/CalculadoraTest.java
@@ -17,13 +17,37 @@ public class CalculadoraTest {
     
     @Test
     /*
-    * Test que verifica si se realizo la suma
+    * Test que verifica si se realizo la suma con numeros positivos
     */
     public void pruebaSumPositivos(){
         System.out.println("iniciando prueba suma de positivos");
         Calculadora c = new Calculadora();
         int resultado = c.suma(2,3);
         int valor_esperado = 5;
+        Assert.assertTrue(resultado==valor_esperado);
+    }
+    
+        @Test
+    /*
+    * Test que verifica si se realizo la suma con numeros negativos
+    */
+    public void pruebaSumNegativo(){
+        System.out.println("iniciando prueba suma de negativos");
+        Calculadora c = new Calculadora();
+        int resultado = c.suma(-2,-3);
+        int valor_esperado = -5;
+        Assert.assertTrue(resultado==valor_esperado);
+    }
+    
+        @Test
+    /*
+    * Test que verifica si se realizo la suma de positivos y negativos
+    */
+    public void pruebaSumPosNeg(){
+        System.out.println("iniciando prueba suma de positivos y negativos");
+        Calculadora c = new Calculadora();
+        int resultado = c.suma(2,-3);
+        int valor_esperado = -1;
         Assert.assertTrue(resultado==valor_esperado);
     }
     
