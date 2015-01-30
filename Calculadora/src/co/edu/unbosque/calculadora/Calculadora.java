c5c5c15030c12fd7f3359ae083ac6d242ff9345e
dromeromo <dromeromo@unbosque.edu.co>
2015-01-30 12:56:00 -0500

    se creo la clase claculadora y el metodo suma

---
+++ Calculadora/src/co/edu/unbosque/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unbosque.calculadora;
+
+/**
+ *
+ * @author Andres
+ */
+public class Calculadora {
+
+    public Calculadora() {
+    }
+    
+    public int suma(int i, int i0) {        
+        int resultado = i+i0;
+        return resultado;      
+}
+    
+    
+    
+}
